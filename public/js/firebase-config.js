0506c87ecc1f3a7308ae606125a8cadaf8ed8af2
yusagullu <yusagullu06@gmail.com>
2026-01-09 18:36:39 +0200

    Firebase-i Probleemi Lahendamine

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -6,9 +6,9 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-s
 
 const firebaseConfig = {
   apiKey: "AIzaSyC52G-pG-QPY0JDxFRADxb2EXXwPyXTUGc",
-  authDomain: "ecoludus-9332a.firebaseapp.com",
-  projectId: "ecoludus-9332a",
-  storageBucket: "ecoludus-9332a.firebasestorage.app",
+  authDomain: "ecoquest-9332a.firebaseapp.com",
+  projectId: "ecoquest-9332a",
+  storageBucket: "ecoquest-9332a.firebasestorage.app",
   messagingSenderId: "9456851024",
   appId: "1:9456851024:web:c01b6bef0fbf4f7ce61955",
   measurementId: "G-QEMD4CHE5S"
